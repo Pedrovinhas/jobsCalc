6585754180b74ed0be4cd2fd8bd04903543bcb9c
Pedro Henrique Vinhas <peuvinhas2@gmail.com>
2021-04-04 13:41:35 -0300

    Aula 2 - Novas peças para o motor :hammer:

--- src/routes.js
+++ src/routes.js
@@ -1,22 +1,193 @@
-const express = require('express');
-const routes = express.Router()
+const express = require("express");
+const routes = express.Router();
 
-const views = __dirname + "/views/"
+const views = __dirname + "/views/";
 
-const profile = {
-    name: "Pedro Henrique Vinhas",
-    avatar: "https://avatars.githubusercontent.com/u/65927588?v=4",
-    "monthly-budget": 3000,
-    "days-per-week": 5,
-    "hours-per-day": 4,
-    "vacation-per-year": 4
-}
+const Profile = {
+    data: {
+        name: "Pedro Henrique Vinhas",
+        avatar: "https://github.com/pedrovinhas.png",
+        "monthly-budget": 3000,
+        "days-per-week": 5,
+        "hours-per-day": 4,
+        "vacation-per-year": 4,
+        "value-hour": 75,
+      },
+
+      controllers: {
+           index(req,res) {
+            return res.render(views + "profile", { profile: Profile.data })
+           },
+
+           update(req, res) {
+               // Req body para pegar os dados
+               const data = req.body
+
+               // Definir quantas semanas tem num ano
+               const weeksPerYear = 52
+
+               // Remover as semanas de férias do ano, para pegar quantas semanas tem em um mês
+               const weeksPerMonth = (weeksPerYear - data["vacation-per-year"])/ 12
+
+               // Quantas horas por semana estou trabalhando
+               const weekTotalHours = data["hours-per-day"] * data["days-per-week"]
+
+               // Total de horas trabalhadas no mês
+               const monthlyTotalHours = weekTotalHours * weeksPerMonth
+
+               // Qual será o valor da minha hora?
+               const valueHour = data["value-hour"] = data["monthly-budget"] / monthlyTotalHours
+
+               Profile.data = {
+                   ...Profile.data,
+                   ...req.body,
+                   "value-hour": valueHour
+               }
+
+               return res.redirect('/profile')
+           }
+      }
+
+} 
+
+const Job = {
+  data: [
+    {
+      id: 1,
+      name: "Pizzaria Guloso",
+      "daily-hours": 2,
+      "total-hours": 1,
+      created_at: Date.now(),
+    },
+    {
+      id: 2,
+      name: "OneTwo Project",
+      "daily-hours": 3,
+      "total-hours": 47,
+      created_at: Date.now(),
+    },
+  ],
+
+  controllers: {
+    index(req, res) {
+      const updatedJobs = Job.data.map((job) => {
+        // ajustes nos job
+        const remaining = Job.services.remainingDays(job);
+        const status = remaining <= 0 ? "done" : "progress";
+
+        return {
+          ...job,
+          remaining,
+          status,
+          budget: Job.services.calculateBudget(job, Profile.data["value-hour"])
+        };
+      });
+
+      return res.render(views + "index", { jobs: updatedJobs });
+    },
+
+    create(req,res) {
+        return res.render(views + "job")
+    },
+
+    save(req, res) {
+      // req body = { name: 'asd', 'daily-hours': '3.1', 'total-hours': '15' }
+      const lastId = Job.data[Job.data.length - 1]?.id || 0;
+
+      Job.data.push({
+        id: lastId + 1,
+        name: req.body.name,
+        "daily-hours": req.body["daily-hours"],
+        "total-hours": req.body["total-hours"],
+        created_at: Date.now(), // atribuindo data de hoje
+      });
+      return res.redirect("/");
+    },
+
+    show(req, res) {
+         
+        const jobId = req.params.id
+
+        const job = Job.data.find(job => Number(job.id) === Number(jobId));
+
+        if(!job) {
+            return res.send('Job not found!')
+        }
+
+        job.budget = Job.services.calculateBudget(job, Profile.data["value-hour"])
+
+        res.render(views + "job-edit", { job })
+    },
+
+    update (req, res) {
+        const jobId = req.params.id
+
+        const job = Job.data.find(job => Number(job.id) === Number(jobId));
+
+        if(!job) {
+            return res.send('Job not found!')
+        }
+
+        const updatedJob = {
+            ...job,
+            name: req.body.name,
+            "total-hours": req.body["total-hours"],
+            "daily-hours": req.body["daily-hours"],
+        }
+
+        Job.data = Job.data.map(job => {
+           
+            if(Number(job.id) === Number(jobId)) {
+                job = updatedJob
+            }
+
+            return job
+        })
+
+        res.redirect('/job/' + jobId)
+
+    },
+
+    delete(req, res) {
+        const jobId = req.params.id
+
+        Job.data = Job.data.filter(job => Number(job.id) !== Number(jobId))
+
+        return res.redirect('/')
+    }
+  },
+
+  services: {
+    remainingDays(job) {
+      // cálculo de tempo restante
+      const remainingDays = (job["total-hours"] / job["daily-hours"]).toFixed();
+
+      const createdDate = new Date(job.created_at);
+      const dueDay = createdDate.getDate() + Number(remainingDays);
+      const dueDateInMs = createdDate.setDate(dueDay);
+
+      const timeDiffInMs = dueDateInMs - Date.now();
+      // transformar milli em dias
+      const dayInMs = 1000 * 60 * 60 * 24;
+      const dayDiff = Math.floor(timeDiffInMs / dayInMs);
+
+      // restam x dias
+      return dayDiff;
+    },
+    calculateBudget: (job, valueHour) => valueHour * job["total-hours"],
+  },
+};
 
 // request, response
-routes.get('/', (req, res) => res.render(views + "index"))
-routes.get('/job', (req, res) => res.render(views + "job"))
-routes.get('/job/edit', (req, res) => res.render(views + "job-edit"))
-routes.get('/profile', (req, res) => res.render(views + "profile", { profile }))
-
+routes.get("/", Job.controllers.index);
+routes.get("/job", Job.controllers.create);
+routes.post("/job", Job.controllers.save);
+routes.get("/job/:id", Job.controllers.show);
+routes.post("/job/:id", Job.controllers.update);
+routes.post("/job/delete/:id", Job.controllers.delete);
+routes.get("/profile", Profile.controllers.index);
+routes.post("/profile", Profile.controllers.update);
 
 module.exports = routes;
+
+  
